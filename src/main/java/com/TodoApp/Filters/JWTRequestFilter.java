63804e44a846575d8683e16db187ed468d29813e
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-16 00:53:46 +0300

    User service

---
+++ src/main/java/com/TodoApp/Filters/JWTRequestFilter.java
@@ -0,0 +1,7 @@
+package com.TodoApp.Filters;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTRequestFilter {
+}
--- src/main/java/com/TodoApp/service/UserAuthDetailsService.java
+++ src/main/java/com/TodoApp/service/UserAuthDetailsService.java
@@ -3,13 +3,9 @@ package com.TodoApp.service;
 import com.TodoApp.model.CustomUserDetails;
 import com.TodoApp.model.User;
 import com.TodoApp.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Optional;
 
 @Service
 public class UserAuthDetailsService implements UserDetailsService {
