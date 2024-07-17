004f0d048a9bb794b47ce9358cc649996949333d
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-17 15:55:51 +0300

    Task controller update

--- src/main/java/com/TodoApp/controller/TaskController.java
+++ src/main/java/com/TodoApp/controller/TaskController.java
@@ -25,38 +25,42 @@ public class TaskController {
         return ResponseEntity.ok(taskService.getAllTasks());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/tasks/{id}")
     public ResponseEntity<Task> getTask(@PathVariable Long id){
         return taskService.getTaskById(id).map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/{taskId}/steps")
-    public ResponseEntity<List<Step>> getSteps(@PathVariable Long taskId){
+    @GetMapping("/tasks/{taskId}/steps")
+    public ResponseEntity<List<Step>> getAllSteps(@PathVariable Long taskId){
         return ResponseEntity.ok(taskService.getSteps(taskId));
     }
-    @GetMapping("/completed")
+    @GetMapping("/tasks/steps/{stepId}")
+    public ResponseEntity<Step> getStep(@PathVariable Long stepId){
+        return taskService.getStepById(stepId).map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+    }
+    @GetMapping("/tasks/completed")
     public ResponseEntity<List<Task>> getCompletedTasks(){
         return ResponseEntity.ok(taskService.getCompletedTasks());
     }
 
-    @PostMapping
+    @PostMapping("/tasks")
     public ResponseEntity<Task> addTask(@RequestBody Task task){
         return new ResponseEntity<>(taskService.createOrUpdateTask(task), HttpStatus.CREATED);
     }
 
-    @PostMapping("/{taskId}/steps")
+    @PostMapping("/tasks/{taskId}/steps")
     public ResponseEntity<Step> addStep(@PathVariable Long taskId, @RequestBody Step step){
         return new ResponseEntity<>(taskService.addStep(taskId, step), HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/tasks/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task){
-        Optional<Task> existing = taskService.getTaskById(id);
-        if (existing.isEmpty()) return ResponseEntity.notFound().build();
+        Optional<Task> old = taskService.getTaskById(id);
+        if (old.isEmpty()) return ResponseEntity.notFound().build();
         task.setId(id);
         return ResponseEntity.ok(taskService.createOrUpdateTask(task));
     }
-    @PutMapping("{taskId}/steps/{stepId}")
+    @PutMapping("/tasks/{taskId}/steps/{stepId}")
     public ResponseEntity<Step> updateStep(@PathVariable Long taskId, @PathVariable Long stepId, @RequestBody Step step){
         Optional<Step> old = taskService.getStepById(stepId);
         if (old.isEmpty()) return ResponseEntity.notFound().build();
@@ -64,13 +68,13 @@ public class TaskController {
         return ResponseEntity.ok(taskService.addStep(taskId, step));
     }
 
-    @DeleteMapping("/steps/{stepId}")
+    @DeleteMapping("/tasks/steps/{stepId}")
     public ResponseEntity<Void> deleteStep(@PathVariable Long stepId){
         taskService.deleteStep( stepId);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/tasks/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
         taskService.deleteTaskById(id);
         return ResponseEntity.noContent().build();
