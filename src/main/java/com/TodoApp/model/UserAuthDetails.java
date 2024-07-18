886cc39de160d3de42cd41eea8a755c88a988f9b
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-18 14:14:41 +0300

    new: auth controller

--- src/main/java/com/TodoApp/model/UserAuthDetails.java
+++ src/main/java/com/TodoApp/model/UserAuthDetails.java
@@ -96,4 +96,12 @@ public class UserAuthDetails implements OAuth2User, UserDetails {
     public String getName() {
         return null;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
